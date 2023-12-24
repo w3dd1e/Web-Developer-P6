2677a6370790e7cd99802946890e84e3d99ddfeb
w3dd1e <kelly.weddle@gmail.com>
2023-12-23 20:46:49 -0600

    Add HTTP server to server.js

--- back/server.js
+++ back/server.js
@@ -1,1 +1,9 @@
+const http = require("http");
 
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World!");
+});
+server.listen(port);
