2d4e5d6b3fbe2e59e2c477244676bbcab832cb0a
w3dd1e <kelly.weddle@gmail.com>
2023-12-23 23:42:52 -0600

    Update server.js to use Express.js

--- back/server.js
+++ back/server.js
@@ -1,12 +1,12 @@
 //HTTP Server
 const http = require("http");
+const app = require("./app");
 
-const port = process.env.PORT || 3000;
+port = process.env.PORT || 3000;
+app.set("port", process.env.PORT || 3000);
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!");
-});
+const server = http.createServer(app);
+
 server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
