682de6b391e28092afcf4773dde0346623d084d2
w3dd1e <kelly.weddle@gmail.com>
2023-12-28 12:46:36 -0600

    Update sauce routes with authentication and multer middleware

--- back/app/routes/sauce.js
+++ back/app/routes/sauce.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 const sauceCtrl = require("../controllers/sauce");
 
-router.get("/api/sauces", sauceCtrl.getAllSauces);
-router.get("/api/sauces/:id", sauceCtrl.getOneSauce);
-router.post("/api/sauces", sauceCtrl.createSauce);
-router.put("/api/sauces/:id", sauceCtrl.updateSauce);
-router.delete("/api/sauces/:id", sauceCtrl.deleteSauce);
+router.get("/api/sauces", auth, sauceCtrl.getAllSauces);
+router.get("/api/sauces/:id", auth, sauceCtrl.getOneSauce);
+router.post("/api/sauces", auth, multer, sauceCtrl.createSauce);
+router.put("/api/sauces/:id", auth, multer, sauceCtrl.updateSauce);
+router.delete("/api/sauces/:id", auth, sauceCtrl.deleteSauce);
 
 module.exports = router;
