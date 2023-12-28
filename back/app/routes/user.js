652e43063db55f4e5c7ec41499a6eaf97874ea55
w3dd1e <kelly.weddle@gmail.com>
2023-12-27 21:32:15 -0600

    Update user routes

--- back/app/routes/user.js
+++ back/app/routes/user.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
 const userCtrl = require("../controllers/user");
 
-router.post("auth/signup", userCtrl.signup);
-router.post("auth/login", userCtrl.login);
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
 
 module.exports = router;
