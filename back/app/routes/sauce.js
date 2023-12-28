f72a480f5c3003d98c4bda2fa7cf61ed6317cf9c
w3dd1e <kelly.weddle@gmail.com>
2023-12-27 22:51:56 -0600

    Add routes for getting, creating, updating, and deleting sauces

--- back/app/routes/sauce.js
+++ back/app/routes/sauce.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 const sauceCtrl = require("../controllers/sauce");
 
 router.get("/api/sauces", sauceCtrl.getAllSauces);
+router.get("/api/sauces/:id", sauceCtrl.getOneSauce);
+router.post("/api/sauces", sauceCtrl.createSauce);
+router.put("/api/sauces/:id", sauceCtrl.updateSauce);
+router.delete("/api/sauces/:id", sauceCtrl.deleteSauce);
 
 module.exports = router;
