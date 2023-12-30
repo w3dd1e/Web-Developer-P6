ce7a8d084561d4e27c52b678dee7d829b16ea81d
w3dd1e <kelly.weddle@gmail.com>
2023-12-30 17:07:52 -0600

    Update sauce routes

--- back/app/routes/sauce.js
+++ back/app/routes/sauce.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
+
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
+
 const sauceCtrl = require("../controllers/sauce");
 
-router.get("/api/sauces", auth, sauceCtrl.getAllSauces);
-router.get("/api/sauces/:id", auth, sauceCtrl.getOneSauce);
+router.get("/api/sauces", auth, sauceCtrl.getAllSauce);
 router.post("/api/sauces", auth, multer, sauceCtrl.createSauce);
+router.get("/api/sauces/:id", auth, sauceCtrl.getOneSauce);
 router.put("/api/sauces/:id", auth, multer, sauceCtrl.updateSauce);
 router.delete("/api/sauces/:id", auth, sauceCtrl.deleteSauce);
 
