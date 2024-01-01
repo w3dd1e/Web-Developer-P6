c85150868c9f17aeb18f22cc3d1c76b1cb454ee2
w3dd1e <kelly.weddle@gmail.com>
2024-01-01 17:52:33 -0600

    Fix missing "required" attribute for mainPepper field

--- back/app/models/sauce.js
+++ back/app/models/sauce.js
@@ -5,7 +5,7 @@ const sauceSchema = mongoose.Schema({
   name: { type: String, required: true },
   manufacturer: { type: String, required: true },
   description: { type: String, required: true },
-  mainPepper: { type: String },
+  mainPepper: { type: String }, required: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
   likes: { type: Number },
