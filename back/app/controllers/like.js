0343c432730fc0620801402496502386751ab043
w3dd1e <kelly.weddle@gmail.com>
2024-01-01 19:24:13 -0600

    Refactor like.js to update like & dislike counts while adding & removing users from related arrays

--- back/app/controllers/like.js
+++ back/app/controllers/like.js
@@ -1,21 +1,49 @@
 const Sauce = require("../models/sauce");
 
 exports.updateLike = (req, res, next) => {
-  sauce = findOne({ _id: req.params.id });
-  userId = req.body.userId;
-  like = req.body.like;
+  let like = req.body.like;
+  let user = req.body.userId;
 
-  sauce
-    .updateOne({ _id: req.params.id }, like, usersLiked.push(userId))
-    .then(() => {
-      res.status(201).json({
-        message: "Likes updated successfully!",
+  Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+    let totalLiked = sauce.usersLiked.length;
+    let totalDisliked = sauce.usersDisliked.length;
+
+    if (like === 1 && !sauce.usersLiked.includes(user)) {
+      totalLiked++;
+      sauce = {
+        $push: { usersLiked: user },
+        likes: totalLiked,
+      };
+    } else if (like === -1 && !sauce.usersDisliked.includes(user)) {
+      totalDisliked++;
+      sauce = {
+        $push: { usersDisliked: user },
+        dislikes: totalDisliked,
+      };
+    } else if (like === 0 && sauce.usersLiked.includes(user)) {
+      totalLiked--;
+      sauce = {
+        $pull: { usersLiked: user },
+        likes: totalLiked,
+      };
+    } else if (like === 0 && sauce.usersDisliked.includes(user)) {
+      totalDisliked--;
+      sauce = {
+        $pull: { usersDisliked: user },
+        dislikes: totalDisliked,
+      };
+    }
+    Sauce.updateOne({ _id: req.params.id }, sauce)
+      .then(() => {
+        res.status(201).json({
+          message: "Like updated successfully!",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(400).json({
+          error: error,
+        });
       });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(400).json({
-        error: error,
-      });
-    });
+  });
 };
